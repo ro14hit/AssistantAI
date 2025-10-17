fb92a9f49c0a5c6dc2e600a306ff1ecaa2d30058
ro14hit <rohit1499singh@gmail.com>
2025-10-17 23:14:17 +0530

    Changes related to geminiapi

--- components/hero.jsx
+++ components/hero.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="_blank">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
